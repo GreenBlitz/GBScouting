4f3ec20697191aaeae1c42fce271893aea38128a
RJ <ranelj2008@gmail.com>
2024-12-01 20:47:26 +0200

    Ran - started using hooks

--- application/src/initialProject/SendMsg.tsx
+++ application/src/initialProject/SendMsg.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 const SendMsg: React.FC=()=>{
+    const [counter, setCounter] = useState(0)
+
     const alertWhenPressed = ()=>{
-        console.log("Yoni")
-}
-    const title = <h2>Please Enter Message</h2>
-    const enterMsgField = <input placeholder="ur mama" />
-    const submitButton = <button onClick={alertWhenPressed} placeholder="ur mamam2" />
+        setCounter(counter+1)
+        console.log(counter)
+    }
+
+    const title = <h2>Please Enter Message {counter}</h2>
+    const enterMsgField = <input onChange={() => {}} placeholder="ur mama"/>
+    const submitButton = <button onClick={alertWhenPressed}>Click Me</button>
+    const writtenMessages = <ul></ul>
     return(
         <>
             {title}
