eb1c4efe8f1a5fff134395033d7cb169fa67a2b3
Yotam7 <76944434+Yotam7@users.noreply.github.com>
2024-12-08 20:20:10 +0200

    Serde 2.0 (#15)
    * Yotam- formatted serde
    
    * Yoni - committing for yotam
    
    * Yotam- finish transition internals to use BitArray, improve user side writing of de/serialization configurations
    
    * Yotam- fixes for cr
    
    * refactor: improve readability of array creation
    
    Signed-off-by: Yotam Cohen <76944434+Yotam7@users.noreply.github.com>
    
    * refactor: fixes for cr
    
    * refactor: place serde in utils, and make serde a ts file
    
    * refactor: move BitArray to seperate file, move rangeArr to Utils.ts
    
    * refactor: use generic T instead of any to improve usability
    
    * reformat: add some spacing
    
    * refactor: write explicit return type into function
    
    * perf: decrease serialized size
    
    ---------
    
    Signed-off-by: Yotam Cohen <76944434+Yotam7@users.noreply.github.com>
    Co-authored-by: emilylime <emilyyyylime+git@gmail.com>

---
+++ application/src/utils/BitArray.ts
@@ -0,0 +1,58 @@
+import { rangeArr } from "../Utils"
+
+export class BitArray {
+  boolArr: boolean[];
+  bitCount: number;
+
+  constructor(arr?: Uint8Array) {
+    this.bitCount = 0;
+    this.boolArr = [];
+    if (arr) {
+      this.insert(arr, arr.length * 8);
+    }
+  }
+
+  insert(data: Uint8Array, totalBitCount: number): void;
+  insert(bitArray: BitArray): void;
+  insert(data: BitArray | Uint8Array, totalBitCount?: number): void {
+    function insertBitArray(bitArr1: BitArray, bitArr2: BitArray) {
+      bitArr1.boolArr = bitArr1.boolArr.concat(bitArr2.boolArr);
+      bitArr1.bitCount += bitArr2.bitCount;
+    }
+    function insertBits(bitArr: BitArray, data: Uint8Array, bitCount: number) {
+      rangeArr(0, bitCount).forEach((i) => {
+        bitArr.boolArr.push((data[Math.floor(i / 8)] & (1 << i % 8)) !== 0);
+      });
+      bitArr.bitCount += bitCount;
+    }
+
+    if (totalBitCount !== undefined) {
+      insertBits(this, data as Uint8Array, totalBitCount);
+    } else {
+      insertBitArray(this, data as BitArray);
+    }
+  }
+
+  consumeBits(bitCount: number): Uint8Array {
+    let returnedArr = new Uint8Array(Math.ceil(bitCount / 8));
+    rangeArr(0, bitCount).forEach((i) => {
+      returnedArr[Math.floor(i / 8)] |= (this.boolArr.shift() ? 1 : 0) << i % 8;
+    });
+    this.bitCount -= bitCount;
+    return returnedArr;
+  }
+
+  insertBool(bool: boolean) {
+    this.boolArr.push(bool);
+    this.bitCount++;
+  }
+
+  consumeBool(): boolean {
+    this.bitCount--;
+    return this.boolArr.shift() as boolean;
+  }
+
+  consume(): Uint8Array {
+    return this.consumeBits(this.bitCount);
+  }
+}
