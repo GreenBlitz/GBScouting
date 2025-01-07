2427db09d0ed62ecd9977d185b49a564be9a5661
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2025-01-07 17:02:11 +0200

    Ran and Yoni-skiiddi-returned it back to normal

--- application/src/main.tsx
+++ application/src/main.tsx
@@ -3,14 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import React from "react";
-import AlgaeButton from "./scouter/AlgaeButton.tsx";
-import CoralButton from "./scouter/CoralButton.tsx";
-import AutonomousForm from "./scouter/AutonomousForm.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <AlgaeButton /> */}
-    <AutonomousForm />
-    {/* <CoralButton /> */}
+    <App />
   </StrictMode>
 );
--- application/src/scouter/tabs/Autonomous.tsx
+++ application/src/scouter/tabs/Autonomous.tsx
@@ -1,25 +1,12 @@
 import React from "react";
 import ScouterInputs from "../ScouterInputs";
+import AutonomousForm from "../AutonomousForm";
 interface AutonomousProps {}
 
 const Autonomous: React.FC<AutonomousProps> = () => {
   return (
     <>
-      <div className="map-buttons">
-        <div className="speaker-auto">
-          <h3>SCORE </h3>
-          <br />
-
-          {ScouterInputs.speakerAutoScore.create()}
-        </div>
-        <div className="speaker-auto">
-          <h3>MISS</h3>
-          <br />
-
-          {ScouterInputs.speakerAutoMiss.create()}
-        </div>
-      </div>
-      <br />
+      <AutonomousForm/>
     </>
   );
 };
