de225cca611d34402d418d62581645055ef326be
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2025-01-07 15:42:01 +0200

    Ran-whasssssssssup

--- application/src/main.tsx
+++ application/src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import React from 'react'
-import AlgaeButton from './scouter/AlgaeButton.tsx'
-import CoralButton from './scouter/CoralButton.tsx'
-import AutonomousForm from './scouter/AutonomousForm.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import React from "react";
+import AlgaeButton from "./scouter/AlgaeButton.tsx";
+import CoralButton from "./scouter/CoralButton.tsx";
+import AutonomousForm from "./scouter/AutonomousForm.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AlgaeButton />
+    {/* <AlgaeButton /> */}
     <AutonomousForm />
-    <CoralButton />
-  </StrictMode>,
-)
+    {/* <CoralButton /> */}
+  </StrictMode>
+);
--- application/src/scouter/AutonomousForm.tsx
+++ application/src/scouter/AutonomousForm.tsx
@@ -1,8 +1,31 @@
 import React, { useState } from "react";
-const AutonomousForm:React.FC = ()=>{
-    const blueAllienceAutonomousMap = <img src="\src\assets\blue-auto-map.png" alt="" style={{width: "40%", height: "40%"}} />
-    return<>
-    {blueAllienceAutonomousMap}
-    </>
-}
+import CoralButton from "./CoralButton";
+import AlgaeButton from "./AlgaeButton";
+
+const AutonomousForm: React.FC = () => {
+  const coralButtons = [<CoralButton />, <CoralButton />, <CoralButton />];
+  const AlgaeButtons = [<AlgaeButton />, <AlgaeButton />, <AlgaeButton />];
+  const buttons = (
+    <div
+      className="buttons"
+      style={{ display: "grid", gridTemplateColumns: "auto auto" }}
+    >
+      {coralButtons.map((CoralButton) => CoralButton)}
+      {AlgaeButtons.map((AlgaeButton) => AlgaeButton)}
+    </div>
+  );
+
+  const blueAllienceAutonomousMap = (
+    <div className="container">
+      <img
+        src="\src\assets\blue-auto-map.png"
+        alt=""
+        style={{ width: "40%", height: "40%" }}
+      ></img>
+      {buttons}
+    </div>
+  );
+
+  return <>{blueAllienceAutonomousMap}</>;
+};
 export default AutonomousForm;
