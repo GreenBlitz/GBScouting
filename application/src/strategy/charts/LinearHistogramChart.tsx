f2e17a8e092a0af17b164a1479f663467460fd7c
Yoni Kiriaty <YoniKiriaty@gmail.com>
2025-01-08 19:24:09 +0200

    Yoni - box

--- application/src/strategy/charts/LinearHistogramChart.tsx
+++ application/src/strategy/charts/LinearHistogramChart.tsx
@@ -1,7 +1,36 @@
-import React from "react"
+import React, { useEffect, useRef } from "react";
+
 interface LinearHistogramProps {
-    
+  width: number;
+  height: number;
 }
-const LinearHistogramChart: React.FC<LinearHistogramProps> = ({}) => {
-    return <></>
-}
+
+const boxThickness = 5;
+
+const LinearHistogramChart: React.FC<LinearHistogramProps> = ({
+  width,
+  height,
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const context = canvasRef.current ? canvasRef.current.getContext("2d") : null;
+  const initializeBox = (context: CanvasRenderingContext2D) => {
+    context.fillRect(0, 0, width, height);
+    context.clearRect(
+      boxThickness,
+      boxThickness,
+      width - boxThickness,
+      height - boxThickness
+    );
+  };
+
+  useEffect(() => {
+    if (context) {
+      initializeBox(context);
+    }
+  });
+  return (
+    <div>
+      <canvas ref={canvasRef} />
+    </div>
+  );
+};
