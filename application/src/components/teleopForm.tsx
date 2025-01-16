b2c68bdf62d171d5e7e32d2657920e5c05efd6e2
Doobani <aviv2727x@gmail.com>
2025-01-16 19:22:34 +0200

    Aviv- Add algea to the teleop form

--- application/src/components/teleopForm.tsx
+++ application/src/components/teleopForm.tsx
@@ -20,36 +20,44 @@ interface NetScore{
     miss: number;
 }
 
-interface NetUndoAction{
+interface NetAction{
     type: keyof NetScore;
 }
-type FullAction = NetUndoAction | CoralAction
+type FullAction = NetAction | CoralAction | AlgeaAction;
 
 interface CoralAction{
     level: keyof Levels;
     point: keyof Level;
 }
+interface Algea{
+  algea: number;
+}
+interface AlgeaAction{
+  type: keyof Algea;
+}
 
-class TeleopForm extends ScouterInput<Levels & NetScore, {}, { levels: Levels; undoStack: FullAction[]; netScore: NetScore}> {
+class TeleopForm extends ScouterInput<Levels & NetScore & Algea, {}, { levels: Levels; undoStack: FullAction[]; netScore: NetScore, algeaScore: Algea }> {
   create(): React.JSX.Element {
     return <TeleopForm {...this.props} />;
   }
 
-  initialValue(): Levels & NetScore {
+  initialValue(): Levels & NetScore & Algea{
     return {
       L1: { score: 0, miss: 0 },
       L2: { score: 0, miss: 0 },
       L3: { score: 0, miss: 0 },
       L4: { score: 0, miss: 0 },
       score: 0, miss: 0 ,
+      algea: 0,
     };
   }
 
-  getStartingState(props: InputProps<Levels>): { levels: Levels; undoStack: FullAction[], netScore: NetScore} {
+  getStartingState(props: InputProps<Levels>): { levels: Levels; undoStack: FullAction[], netScore: NetScore, algeaScore: Algea } {
     return {
       levels: this.initialValue(),
       undoStack: [],
-      netScore: {score: 0, miss: 0}
+      netScore: {score: 0, miss: 0},
+      algeaScore: {algea: 0}
     };
   }
 
@@ -63,73 +71,112 @@ class TeleopForm extends ScouterInput<Levels & NetScore, {}, { levels: Levels; u
       const updatedLevels = { ...this.state.levels };
       updatedLevels[action.level][action.point] += 1;
       this.setState({undoStack: [...this.state.undoStack, { level: action.level, point: action.point }]});
-      this.storage.set({...updatedLevels, score: this.state.netScore.score, miss: this.state.netScore.miss});
+      this.storage.set({...updatedLevels, score: this.state.netScore.score, miss: this.state.netScore.miss, ...this.state.algeaScore});
     };
     
-    const handleNet = (action: NetUndoAction) => {
+    const handleNet = (action: NetAction) => {
         const updatedNetScore = { ...this.state.netScore };
         updatedNetScore[action.type] += 1;
         this.setState({undoStack: [...this.state.undoStack, action]});
         this.setState({netScore: updatedNetScore});
-        this.storage.set({...this.state.levels, ...updatedNetScore});
+        this.storage.set({...this.state.levels, ...updatedNetScore, ...this.state.algeaScore});
       };
 
+      const handleAlgea = (action: AlgeaAction) => {
+        const updatedAlgeaScore = { ...this.state.algeaScore};
+        updatedAlgeaScore[action.type] += 1;
+        this.setState({undoStack: [...this.state.undoStack, action]});
+        this.setState({algeaScore: updatedAlgeaScore});
+        this.storage.set({...this.state.levels, ...this.state.netScore, ...updatedAlgeaScore});
 
-    const handleUndo = () => {
-      if (this.state.undoStack.length === 0) return;
 
-      const lastAction = this.state.undoStack[this.state.undoStack.length - 1];
-      const updatedNetScore = { ...this.state.netScore };
-      const updatedLevels = { ...this.state.levels };
-      if('level' in lastAction){
-        
-        updatedLevels[lastAction.level][lastAction.point] -= 1;
-        this.setState({ levels: updatedLevels, undoStack: this.state.undoStack.slice(0, -1) });
-        }else{
-            
-            updatedNetScore[lastAction.type] -= 1;
-            this.setState({ netScore: updatedNetScore, undoStack: this.state.undoStack.slice(0, -1) });
+      }
+
+
+
+      const handleUndo = () => {
+        if (this.state.undoStack.length === 0) return;
+      
+        const lastAction = this.state.undoStack[this.state.undoStack.length - 1];
+        const updatedNetScore = { ...this.state.netScore };
+        const updatedLevels = { ...this.state.levels };
+        const updatedAlgea = { ...this.state.algeaScore };
+      
+        if ("level" in lastAction) {
+          updatedLevels[lastAction.level][lastAction.point] -= 1;
+          this.setState({ levels: updatedLevels });
+        } else if ("type" in lastAction && lastAction.type === "score" || lastAction.type === "miss") {
+          updatedNetScore[lastAction.type] -= 1;
+          this.setState({ netScore: updatedNetScore });
+        } else if ("type" in lastAction && lastAction.type === "algea") {
+          updatedAlgea.algea -= 1;
+          this.setState({ algeaScore: updatedAlgea });
         }
-
       
-
+        this.setState({
+          undoStack: this.state.undoStack.slice(0, -1),
+        });
       
-
-      this.storage.set({...updatedLevels, ...updatedNetScore});
-    };
+        this.storage.set({ ...updatedLevels, ...updatedNetScore, ...updatedAlgea });
+      };
 
     return (
-        
-      <div>
-        <h1>Reef Scoring</h1>
-        {levelKeys.map((levelKey) => {
-          const level = levelKey as keyof Levels;
-          return (
-            <div key={level}>
-              <h2>{level}</h2>
-              <button
-                className="buttonS"
-                onClick={() => handleClick({level:level, point: "score"})}
-              >
-                {this.state.levels[level].score}
-              </button>
-              <button
-                className="buttonF"
-                onClick={() => handleClick({level:level, point: "miss"})}
-              >
-                {this.state.levels[level].miss}
-              </button>
-            </div>
-          );
-        })}
-        <h2>Net Score</h2>
-        <button className="buttonS" onClick={() => handleNet({type: "score"})}>{this.state.netScore.score} </button>
-        <button className="buttonF" onClick={() => handleNet({type: "miss"})}> {this.state.netScore.miss}</button>
-        <button className="buttonU" onClick={handleUndo}>
-          Undo
-        </button>
-      </div>
-    );
+  <div className="container">
+    <h1>Reef Scoring</h1>
+    {levelKeys.map((levelKey) => {
+      const level = levelKey as keyof Levels;
+      return (
+        <div className="section" key={level}>
+          <h2>{level}</h2>
+          <button
+            className="buttonS"
+            onClick={() => handleClick({ level: level, point: "score" })}
+          >
+            {this.state.levels[level].score}
+          </button>
+          <button
+            className="buttonF"
+            onClick={() => handleClick({ level: level, point: "miss" })}
+          >
+            {this.state.levels[level].miss}
+          </button>
+        </div>
+      );
+    })}
+    <div className="section">
+      <h2>Net Score</h2>
+      <button
+        className="buttonS"
+        onClick={() => handleNet({ type: "score" })}
+      >
+        {this.state.netScore.score}
+      </button>
+      <button
+        className="buttonF"
+        onClick={() => handleNet({ type: "miss" })}
+      >
+        {this.state.netScore.miss}
+      </button>
+    </div>
+    <div className="section">
+      <h2>Algae</h2>
+      <button
+        className="buttonS"
+        onClick={() => handleAlgea({ type: "algea" })}
+      >
+        {this.state.algeaScore.algea}
+      </button>
+      <button className="buttonU" onClick={handleUndo}>
+        Undo
+      </button>
+    </div>
+    <div className="section">
+      <button className="buttonU">Back</button>
+      <button className="buttonU">Next</button>
+    </div>
+  </div>
+);
+
   }
 }
 
