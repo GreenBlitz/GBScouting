c529bac969d58aed962910963090d8d8e2c2bc62
greenblitz <greenblitz@gmail.com>
2025-01-29 16:22:41 +0200

    Yoni - removed example data

--- application/src/strategy/team-tab/TeamTab.tsx
+++ application/src/strategy/team-tab/TeamTab.tsx
@@ -7,93 +7,6 @@ import { renderStrategyNavBar } from "../../App";
 import { fetchMatchesByCriteria } from "../../utils/Fetches";
 import { Link, Outlet } from "react-router-dom";
 
-const exampleData: TeamData = new TeamData([
-  {
-    scouterName: "",
-    qual: 0,
-    teamNumber: 0,
-    gameSide: "",
-    startingPosition: "",
-    noShow: false,
-    defense: 4,
-    climb: "Off Barge",
-    comment: "",
-    autoMap: {
-      Sushi1: {
-        coral: false,
-        algea: false,
-      },
-      Sushi2: {
-        coral: true,
-        algea: false,
-      },
-      Sushi3: {
-        coral: false,
-        algea: false,
-      },
-    },
-    autoCollect: 0,
-    autoReef: {
-      L1: { score: 0, miss: 0 },
-      L2: { score: 0, miss: 0 },
-      L3: { score: 0, miss: 0 },
-      L4: { score: 0, miss: 1 },
-      net: { score: 0, miss: 0 },
-      proccessor: 0,
-    },
-    teleopReef: {
-      L1: { score: 0, miss: 0 },
-      L2: { score: 5, miss: 2 },
-      L3: { score: 2, miss: 0 },
-      L4: { score: 0, miss: 1 },
-      net: { score: 3, miss: 0 },
-      proccessor: 0,
-    },
-  },
-  {
-    scouterName: "",
-    qual: 1,
-    teamNumber: 0,
-    gameSide: "",
-    startingPosition: "",
-    noShow: false,
-    defense: 3,
-    climb: "Park",
-    comment: "",
-    autoMap: {
-      Sushi1: {
-        coral: false,
-        algea: true,
-      },
-      Sushi2: {
-        coral: false,
-        algea: false,
-      },
-      Sushi3: {
-        coral: false,
-        algea: false,
-      },
-    },
-    autoCollect: 2,
-    autoReef: {
-      L1: { score: 0, miss: 0 },
-      L2: { score: 2, miss: 0 },
-      L3: { score: 0, miss: 0 },
-      L4: { score: 0, miss: 1 },
-      net: { score: 0, miss: 0 },
-      proccessor: 0,
-    },
-    teleopReef: {
-      L1: { score: 5, miss: 1 },
-      L2: { score: 4, miss: 2 },
-      L3: { score: 2, miss: 3 },
-      L4: { score: 3, miss: 1 },
-      net: { score: 2, miss: 1 },
-      proccessor: 4,
-    },
-  },
-]);
-
 const TeamTab: React.FC = () => {
   const [matches, setMatches] = useState<Match[]>([]);
   const [recency, setRecency] = useState<number>(0);
@@ -103,8 +16,8 @@ const TeamTab: React.FC = () => {
     recentMatches.splice(0, recentMatches.length - recency);
   }
 
-  // const teamData = new TeamData(recentMatches);
-  const teamData = exampleData;
+  const teamData = new TeamData(recentMatches);
+  
   return (
     <div className="strategy-app">
       {renderStrategyNavBar()}
