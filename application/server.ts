1adab12d48aa90229a3eaf0d463fb886b2df0d70
Doobani <aviv2727x@gmail.com>
2025-02-09 13:35:31 +0200

    Aviv- fix the CR im the goat

--- application/server.ts
+++ application/server.ts
@@ -11,7 +11,7 @@ const app = express();
 const hostname = "0.0.0.0";
 const port = 4590;
 
-const dirName = "";
+const dirName = process.env.PRODUCTION ? "/app" : "";
 
 // SSL options for HTTPS
 let sslOptions;
