5a3adcfa8e63fb511cdc2f04bc3903c973b87332
RJ0907 <ranelj2008@gmail.com>
2025-03-06 22:22:29 +0200

    Ran - ready for PR

--- application/sw.js
+++ application/sw.js
@@ -1,15 +1,14 @@
 const PRECACHE = 'precache';
 
-// A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
-  './', // Alias for index.html
+  './', 
   'styles.css',
   'script.js',
+  'main.tsx',
   'manifest.json',
 ];
 
-// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(PRECACHE)
@@ -18,7 +17,6 @@ self.addEventListener('install', event => {
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
   const currentCaches = [PRECACHE];
   event.waitUntil(
@@ -33,28 +31,22 @@ self.addEventListener('activate', event => {
 });
 
 
-// On fetch, use cache but update the entry with the latest contents from the server.
 self.addEventListener('fetch', event=> {
-  // If the request doesn't come from the same origin as the referrer, don't store it
   if (!event.request.url.includes(event.request.referrer)){
-    return; // You can also remove the return statement if you want every request to be cached
+    return;
   }
-  // You can use respondWith() to answer ASAP…
   event.respondWith(fromCache(event.request));
-  // ...and waitUntil() to prevent the worker to be killed until the cache is updated.
   event.waitUntil(
     update(event.request)
   );
 });
 
-// Open the cache where the assets were stored and search for the requested resource. Notice that in case of no matching, the promise still resolves but it does with undefined as value.
 function fromCache(request) {
   return caches.open(PRECACHE).then(cache=> {
     return cache.match(request);
   });
 }
 
-// Update consists in opening the cache, performing a network request and storing the new response data.
 function update(request) {
   return caches.open(PRECACHE).then(cache=> {
     return fetch(request).then(function(response) {
