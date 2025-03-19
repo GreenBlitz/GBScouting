ed63f814e7fc37194173aa818a424472d91bbcde
RJ0907 <ranelj2008@gmail.com>
2025-03-19 20:40:34 +0200

    Ran - it works!!!!

--- application/src/scouter/input-types/TeamNumberInput.tsx
+++ application/src/scouter/input-types/TeamNumberInput.tsx
@@ -7,12 +7,14 @@ interface TwoOptions<T, P extends T[]> {
     options1: P;
     options2: P;
 }
+
 interface TwoOptionAndTeamNumber<T extends string> {
     option1: T;
     option2: T;
-    qualNumber: number
+    qualNumber: number;
     teamNumber: number;
 }
+
 const allMatches = await fetchAllAwaitingMatches();
 console.log(allMatches); 
 
@@ -37,115 +39,126 @@ const getTeamNumberByCretria = async <Option extends string>(qualNumber: number,
     }
 }
 
+class TeamNumberInput<Option extends string, Options extends Option[]> extends ScouterInput<TwoOptionAndTeamNumber<Option>, { twoOptions: TwoOptions<Option, Options> }> {
 
-class TeamNumberInput<Option extends string, Options extends Option[]> extends ScouterInput<TwoOptionAndTeamNumber<Option>,{ twoOptions: TwoOptions<Option, Options> }> {
     create(): React.JSX.Element {
-        console.log(fetchAllAwaitingMatches())
-        return <TeamNumberInput {...this.props}/>;
+        console.log(fetchAllAwaitingMatches());
+        return <TeamNumberInput {...this.props} />;
     }
 
     renderInput(): React.ReactNode {
         return (
-        <div>
-            <input
-            type="number"
-            id={this.storage.name}
-            name={this.storage.name}
-            required={this.props.required}
-            defaultValue={this.getValue()?.qualNumber ?? (9999)}
-            onChange={(event) => {
-                let storedTwoOptionAndNumber = this.storage.get();
-                    if (storedTwoOptionAndNumber) {
-                        storedTwoOptionAndNumber = {
-                            option1: storedTwoOptionAndNumber.option1,
-                            option2: storedTwoOptionAndNumber.option2,
-                            qualNumber: event.target.value as unknown as number,
-                            teamNumber: 4590
-                        }
-                        this.storage.set({
-                            ...storedTwoOptionAndNumber,
-                            teamNumber: getTeamNumberByCretria(
-                                storedTwoOptionAndNumber.qualNumber, 
+            <div>
+                <input
+                    type="number"
+                    id={this.storage.name}
+                    name={this.storage.name}
+                    required={this.props.required}
+                    defaultValue={this.getValue()?.qualNumber ?? 9999}
+                    onChange={async (event) => {
+                        let storedTwoOptionAndNumber = this.storage.get();
+                        if (storedTwoOptionAndNumber) {
+                            storedTwoOptionAndNumber = {
+                                option1: storedTwoOptionAndNumber.option1,
+                                option2: storedTwoOptionAndNumber.option2,
+                                qualNumber: event.target.value as unknown as number,
+                                teamNumber: 4590, // Temporary value before update
+                            };
+
+                            // Await the resolved team number
+                            const updatedTeamNumber = await getTeamNumberByCretria(
+                                storedTwoOptionAndNumber.qualNumber,
                                 storedTwoOptionAndNumber.option1,
                                 storedTwoOptionAndNumber.option2
-                            )
-                        });
-                    }
-                    console.log(this.storage.get())
-            }}
-            className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
-            />
-                <select
-                name={this.storage.name}
-                id={this.storage.name}
-                required={this.props.required}
-                value={this.getValue()?.option1 ?? "Ran 1"}
-                onChange={(event) => {
-                    let storedTwoOptionAndNumber = this.storage.get();
-                    if (storedTwoOptionAndNumber) {
-                        storedTwoOptionAndNumber = {
-                            option1: event.target.value as Option,
-                            option2: storedTwoOptionAndNumber.option2,
-                            qualNumber: storedTwoOptionAndNumber.qualNumber,
-                            teamNumber: 4590
+                            );
+
+                            this.storage.set({
+                                ...storedTwoOptionAndNumber,
+                                teamNumber: updatedTeamNumber, // Set the resolved team number
+                            });
                         }
-                        this.storage.set({
-                            ...storedTwoOptionAndNumber,
-                            teamNumber: getTeamNumberByCretria(
-                                storedTwoOptionAndNumber.qualNumber, 
-                                storedTwoOptionAndNumber.option1,
-                                storedTwoOptionAndNumber.option2
-                            )                          
-                        });
-                    }
-                    console.log(this.storage.get())
-                }}
-                className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
-                >
-                {this.props.twoOptions.options1.map((item, index) => (
-                    <option value={item} key={index} className="bg-dark-bg text-dark-text">
-                    {item}
-                    </option>
-                ))}
-                </select>
-
-
+                        console.log(this.storage.get());
+                    }}
+                    className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
+                />
                 
                 <select
-                name={this.storage.name}
-                id={this.storage.name}
-                required={this.props.required}
-                value={this.getValue()?.option2 ?? "Ran 2"}
-                onChange={(event) => {
-                    let storedTwoOptionAndNumber = this.storage.get();
-                    if (storedTwoOptionAndNumber) {
-                        storedTwoOptionAndNumber = {
-                            option1: storedTwoOptionAndNumber.option1,
-                            option2: event.target.value as Option as Option,
-                            qualNumber: storedTwoOptionAndNumber.qualNumber,
-                            teamNumber: 4590
-                        }
-                        this.storage.set({
-                            ...storedTwoOptionAndNumber,
-                            teamNumber: getTeamNumberByCretria(
-                                storedTwoOptionAndNumber.qualNumber, 
+                    name={this.storage.name}
+                    id={this.storage.name}
+                    required={this.props.required}
+                    value={this.getValue()?.option1 ?? "Ran 1"}
+                    onChange={async (event) => {
+                        let storedTwoOptionAndNumber = this.storage.get();
+                        if (storedTwoOptionAndNumber) {
+                            storedTwoOptionAndNumber = {
+                                option1: event.target.value as Option,
+                                option2: storedTwoOptionAndNumber.option2,
+                                qualNumber: storedTwoOptionAndNumber.qualNumber,
+                                teamNumber: 4590, // Temporary value before update
+                            };
+
+                            // Await the resolved team number
+                            const updatedTeamNumber = await getTeamNumberByCretria(
+                                storedTwoOptionAndNumber.qualNumber,
                                 storedTwoOptionAndNumber.option1,
                                 storedTwoOptionAndNumber.option2
-                            )
-                        });
-                    }
-                    console.log(this.storage.get())
-                }}
-                className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
+                            );
+
+                            this.storage.set({
+                                ...storedTwoOptionAndNumber,
+                                teamNumber: updatedTeamNumber, // Set the resolved team number
+                            });
+                        }
+                        console.log(this.storage.get());
+                    }}
+                    className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
                 >
-                {this.props.twoOptions.options2.map((item, index) => (
-                    <option value={item} key={index} className="bg-dark-bg text-dark-text">
-                    {item}
-                    </option>
-                ))}
+                    {this.props.twoOptions.options1.map((item, index) => (
+                        <option value={item} key={index} className="bg-dark-bg text-dark-text">
+                            {item}
+                        </option>
+                    ))}
+                </select>
+
+                <select
+                    name={this.storage.name}
+                    id={this.storage.name}
+                    required={this.props.required}
+                    value={this.getValue()?.option2 ?? "Ran 2"}
+                    onChange={async (event) => {
+                        let storedTwoOptionAndNumber = this.storage.get();
+                        if (storedTwoOptionAndNumber) {
+                            storedTwoOptionAndNumber = {
+                                option1: storedTwoOptionAndNumber.option1,
+                                option2: event.target.value as Option,
+                                qualNumber: storedTwoOptionAndNumber.qualNumber,
+                                teamNumber: 4590, // Temporary value before update
+                            };
+
+                            // Await the resolved team number
+                            const updatedTeamNumber = await getTeamNumberByCretria(
+                                storedTwoOptionAndNumber.qualNumber,
+                                storedTwoOptionAndNumber.option1,
+                                storedTwoOptionAndNumber.option2
+                            );
+
+                            this.storage.set({
+                                ...storedTwoOptionAndNumber,
+                                teamNumber: updatedTeamNumber, // Set the resolved team number
+                            });
+                        }
+                        console.log(this.storage.get());
+                    }}
+                    className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
+                >
+                    {this.props.twoOptions.options2.map((item, index) => (
+                        <option value={item} key={index} className="bg-dark-bg text-dark-text">
+                            {item}
+                        </option>
+                    ))}
                 </select>
             </div>
-          );
+        );
     }
 
     initialValue(
