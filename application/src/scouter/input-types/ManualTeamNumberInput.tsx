423ffea7da8408408fc9d0e5bfaa9f68d51d958d
RJ0907 <ranelj2008@gmail.com>
2025-03-23 22:17:11 +0200

    Ran - now works

--- application/src/scouter/input-types/ManualTeamNumberInput.tsx
+++ application/src/scouter/input-types/ManualTeamNumberInput.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ScouterInput, { InputProps } from "../ScouterInput";
-import { fetchAllAwaitingMatches, fetchAllTeamMatches } from "../../utils/Fetches";
-import { Match } from "../../utils/Match";
+import { fetchAllAwaitingMatches } from "../../utils/Fetches";
 
+// Interfaces for the component's data structure
 interface TwoOptions<T, P extends T[]> {
     options1: P;
     options2: P;
@@ -15,14 +15,22 @@ interface TwoOptionAndTeamNumber<T extends string> {
     teamNumber: number;
 }
 
-const allMatches = await fetchAllAwaitingMatches();
-console.log(allMatches); 
+// Define state interface
+interface ManualTeamNumberInputState {
+    isDisabled: boolean;
+    manualTeamNumber: number;
+}
 
-const getTeamNumberByCretria = async <Option extends string>(qualNumber: number, option1: Option, option2: Option) => {
+// Function to fetch the team number based on criteria
+const getTeamNumberByCriteria = async <Option extends string>(
+    qualNumber: number,
+    option1: Option,
+    option2: Option
+) => {
     const allMatches = await fetchAllAwaitingMatches();
     if (!allMatches || !allMatches[qualNumber - 1]) {
-        console.error('Match data is unavailable');
-        return 4590;  
+        console.error("Match data is unavailable");
+        return 4590;
     }
 
     const match = allMatches[qualNumber - 1];
@@ -37,158 +45,150 @@ const getTeamNumberByCretria = async <Option extends string>(qualNumber: number,
         default:
             return 4590;
     }
-}
+};
 
-class ManualTeamNumberInput<Option extends string, Options extends Option[]> extends ScouterInput<TwoOptionAndTeamNumber<Option>, { twoOptions: TwoOptions<Option, Options> }> {
-
+class ManualTeamNumberInput<
+    Option extends string,
+    Options extends Option[]
+> extends ScouterInput<
+    TwoOptionAndTeamNumber<Option>,
+    { twoOptions: TwoOptions<Option, Options> },
+    ManualTeamNumberInputState
+> {
     create(): React.JSX.Element {
-        console.log(fetchAllAwaitingMatches());
-        return <ManualTeamNumberInput {...this.props} />;
+        return <ManualTeamNumberInput {...this.props}/>
     }
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isDisabled: false,
+            manualTeamNumber: this.getValue()?.teamNumber ?? 9999,
+        };
+    }
+
+    handleTeamNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (!this.state.isDisabled) {
+            const newTeamNumber = Number(event.target.value);
+            this.setState({ manualTeamNumber: newTeamNumber });
+
+            let storedTwoOptionAndNumber = this.storage.get();
+            if (storedTwoOptionAndNumber) {
+                storedTwoOptionAndNumber.teamNumber = newTeamNumber;
+                this.storage.set(storedTwoOptionAndNumber);
+            }
+        }
+    };
+
+    toggleDisable = () => {
+        this.setState((prevState) => ({
+            isDisabled: !prevState.isDisabled,
+        }));
+    };
+
+    handleSelectionChange = async (
+        field: "qualNumber" | "option1" | "option2",
+        value: string | number
+    ) => {
+        let storedTwoOptionAndNumber = this.storage.get();
+        if (storedTwoOptionAndNumber) {
+            storedTwoOptionAndNumber = {
+                ...storedTwoOptionAndNumber,
+                [field]: value,
+                teamNumber: 4590,
+            };
+
+            const updatedTeamNumber = await getTeamNumberByCriteria(
+                storedTwoOptionAndNumber.qualNumber,
+                storedTwoOptionAndNumber.option1,
+                storedTwoOptionAndNumber.option2
+            );
+
+            this.storage.set({
+                ...storedTwoOptionAndNumber,
+                teamNumber: updatedTeamNumber,
+            });
+
+            this.setState({ manualTeamNumber: updatedTeamNumber });
+        }
+    };
 
     renderInput(): React.ReactNode {
         return (
-            <div style={{width:"80%"}}>
-                
+            <div style={{ width: "80%" }}>
+                {/* Qual Number Input */}
                 <div>
                     <h3>Qual Number</h3>
-                <input
-                    type="number"
-                    id={this.storage.name}
-                    name={this.storage.name}
-                    required={this.props.required}
-                    defaultValue={this.getValue()?.qualNumber ?? 9999}
-                    onChange={async (event) => {
-                        let storedTwoOptionAndNumber = this.storage.get();
-                        if (storedTwoOptionAndNumber) {
-                            storedTwoOptionAndNumber = {
-                                option1: storedTwoOptionAndNumber.option1,
-                                option2: storedTwoOptionAndNumber.option2,
-                                qualNumber: event.target.value as unknown as number,
-                                teamNumber: 4590, 
-                            };
-
-                            const updatedTeamNumber = await getTeamNumberByCretria(
-                                storedTwoOptionAndNumber.qualNumber,
-                                storedTwoOptionAndNumber.option1,
-                                storedTwoOptionAndNumber.option2
-                            );
-
-                            this.storage.set({
-                                ...storedTwoOptionAndNumber,
-                                teamNumber: updatedTeamNumber, 
-                            });
+                    <input
+                        type="number"
+                        required={this.props.required}
+                        defaultValue={this.getValue()?.qualNumber ?? 9999}
+                        onChange={(event) =>
+                            this.handleSelectionChange("qualNumber", Number(event.target.value))
                         }
-                        console.log(this.storage.get());
-                    }}
-                    className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
-                />
+                        className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
+                    />
                 </div>
-                
+
+                {/* Starting Position Select */}
                 <div>
                     <h3>Starting Position</h3>
-                <select
-                    name={this.storage.name}
-                    id={this.storage.name}
-                    required={this.props.required}
-                    value={this.getValue()?.option1 ?? "Ran 1"}
-                    onChange={async (event) => {
-                        let storedTwoOptionAndNumber = this.storage.get();
-                        if (storedTwoOptionAndNumber) {
-                            storedTwoOptionAndNumber = {
-                                option1: event.target.value as Option,
-                                option2: storedTwoOptionAndNumber.option2,
-                                qualNumber: storedTwoOptionAndNumber.qualNumber,
-                                teamNumber: 4590, 
-                            };
-
-                            const updatedTeamNumber = await getTeamNumberByCretria(
-                                storedTwoOptionAndNumber.qualNumber,
-                                storedTwoOptionAndNumber.option1,
-                                storedTwoOptionAndNumber.option2
-                            );
-
-                            this.storage.set({
-                                ...storedTwoOptionAndNumber,
-                                teamNumber: updatedTeamNumber,
-                            });
+                    <select
+                        required={this.props.required}
+                        value={this.getValue()?.option1 ?? ""}
+                        onChange={(event) =>
+                            this.handleSelectionChange("option1", event.target.value)
                         }
-                        this.forceUpdate();  // <---- Force component to re-render
-                        console.log(this.storage.get());
-                    }}
-                    className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
-                >
-                    {this.props.twoOptions.options1.map((item, index) => (
-                        <option value={item} key={index} className="bg-dark-bg text-dark-text">
-                            {item}
-                        </option>
-                    ))}
-                </select>
+                        className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
+                    >
+                        {this.props.twoOptions.options1.map((item, index) => (
+                            <option value={item} key={index} className="bg-dark-bg text-dark-text">
+                                {item}
+                            </option>
+                        ))}
+                    </select>
                 </div>
 
+                {/* Alliance Color Select */}
                 <div>
                     <h3>Alliance Color</h3>
-                <select
-                    name={this.storage.name}
-                    id={this.storage.name}
-                    required={this.props.required}
-                    value={this.getValue()?.option2 ?? "Ran 2"}
-                    onChange={async (event) => {
-                        let storedTwoOptionAndNumber = this.storage.get();
-                        if (storedTwoOptionAndNumber) {
-                            const newStoredValue = { 
-                                ...storedTwoOptionAndNumber, 
-                                option2: event.target.value as Option, 
-                                teamNumber: 4590 
-                            };
-                    
-                            this.storage.set(newStoredValue);
-                    
-                            const updatedTeamNumber = await getTeamNumberByCretria(
-                                newStoredValue.qualNumber,
-                                newStoredValue.option1,
-                                newStoredValue.option2
-                            );
-                            this.storage.set({ ...newStoredValue, teamNumber: updatedTeamNumber });
-                            this.forceUpdate();  
+                    <select
+                        required={this.props.required}
+                        value={this.getValue()?.option2 ?? ""}
+                        onChange={(event) =>
+                            this.handleSelectionChange("option2", event.target.value)
                         }
-                        console.log(this.storage.get());
-                    }}
-                    className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
-                >
-                    {this.props.twoOptions.options2.map((item, index) => (
-                        <option value={item} key={index} className="bg-dark-bg text-dark-text">
-                            {item}
-                        </option>
-                    ))}
-                </select>
+                        className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
+                    >
+                        {this.props.twoOptions.options2.map((item, index) => (
+                            <option value={item} key={index} className="bg-dark-bg text-dark-text">
+                                {item}
+                            </option>
+                        ))}
+                    </select>
                 </div>
 
+                {/* Team Number Input */}
                 <div>
                     <h3>Team Number</h3>
-                    <input
-                    type="number"
-                    id={this.storage.name}
-                    name={this.storage.name}
-                    required={this.props.required}
-                    defaultValue={this.getValue()?.teamNumber ?? 9999}
-                    onChange={async (event) => {
-                        let storedTwoOptionAndNumber = this.storage.get();
-                        if (storedTwoOptionAndNumber) {
-                            storedTwoOptionAndNumber = {
-                                option1: storedTwoOptionAndNumber.option1,
-                                option2: storedTwoOptionAndNumber.option2,
-                                qualNumber: storedTwoOptionAndNumber.qualNumber,
-                                teamNumber: event.target.value as unknown as number
-                            };
-                            this.storage.set({
-                                ...storedTwoOptionAndNumber, 
-                            });
-                        }
-                        console.log(this.storage.get());
-                    }}
-                    className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
-                />
+                    <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+                        <input
+                            type="number"
+                            required={this.props.required}
+                            value={this.state.manualTeamNumber}
+                            onChange={this.handleTeamNumberChange}
+                            disabled={this.state.isDisabled}
+                            className="w-full p-2 bg-dark-bg text-dark-text border border-dark-border rounded focus:ring-2 focus:ring-primary-500 focus:border-transparent"
+                        />
+                        <button
+                            type="button"
+                            onClick={this.toggleDisable}
+                            className={`p-2 text-white rounded shadow ${
+                                this.state.isDisabled ? "bg-green-500 hover:bg-green-600" : "bg-red-500 hover:bg-red-600"
+                            }`}
+                        >
+                            {this.state.isDisabled ? "Enable" : "Disable"}
+                        </button>
+                    </div>
                 </div>
             </div>
         );
@@ -202,8 +202,8 @@ class ManualTeamNumberInput<Option extends string, Options extends Option[]> ext
         return {
             option1: props.twoOptions.options1[0] ?? ("" as Option),
             option2: props.twoOptions.options2[0] ?? ("" as Option),
-            teamNumber: 9999, 
-            qualNumber: 9999
+            qualNumber: 9999,
+            teamNumber: 9999,
         };
     }
 }
