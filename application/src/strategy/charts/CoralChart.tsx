7239bfd778472860e852340d4b017c65394e6e67
Yoni Kiriaty <YoniKiriaty@gmail.com>
2025-09-07 16:57:39 +0300

    Ran new UI (#103)
    * Ran - Uga Buga
    
    * Ran - Uga Buga 2 (It is now well centered)
    
    * Yoni - fixed stupid
    
    ---------
    
    Co-authored-by: RJ0907 <ranelj2008@gmail.com>

--- application/src/strategy/charts/CoralChart.tsx
+++ application/src/strategy/charts/CoralChart.tsx
@@ -74,7 +74,13 @@ const CoralChart: React.FC<CoralChartProps> = ({ corals }) => {
     }
   }, [coralElements]);
 
-  return <>{coralElements}</>;
+  return <>
+    <div className="flex justify-center">
+  <div className="grid grid-cols-2 gap-4">
+    {coralElements}
+  </div>
+</div>
+  </>;;
 };
 
 export default CoralChart;
